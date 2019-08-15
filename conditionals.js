3729eb98c49981d473bb7ff1e9ba0d88b9175965
Tunde Omotayo <omotayobabatundee@gmail.com>
2019-08-15 12:42:43 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,22 @@
+const raining = false;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+else if(!raining) {
+  console.log("Leave your umbrella at home!");
+}
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} 
+
 const temperature = 30;
 
-if (temperature < 0) {
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+else if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
   console.log("Short sleeves won't cut it!");
