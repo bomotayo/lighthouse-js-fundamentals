d30c4837e3e8a4efabec0aa6bf5e255651e56af8
Tunde Omotayo <omotayobabatundee@gmail.com>
2019-08-17 16:15:47 +0000

    Function that determines someone's age

---
+++ age-calculator.js
@@ -0,0 +1,6 @@
+function ageCalculator(name, yearOfBirth, currentYear){
+  return `${name} is ${currentYear - yearOfBirth} years old.`;
+}
+
+console.log(ageCalculator("Miranda", 1993, 2019));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
